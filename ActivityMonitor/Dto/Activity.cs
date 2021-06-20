4ea2c4b73c5a50b81e22da8940f701d777d6eff6
Gideon Jacobus <Gideondeonjacobus@Gmail.com>
2021-06-20 19:42:37 +0200

    Initial commit.

---
+++ ActivityMonitor/Dto/Activity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityMonitor.Dto
+{
+    public class Activity
+    {
+        public string activeWindowTitle { get; set; }
+        public DateTime activatedWindowDateTime { get; set; }
+        public DateTime windowActiveEndDateTime { get; set; }
+        public TimeSpan windowActiveTimeSpan { get; set; }
+    }
+}
---
+++ ActivityMonitor/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ActivityMonitor
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        static void Main(string[] args)
+        {
+            string activeWindowTitle, previousActiveWindowTitle = "";
+            activeWindowTitle = GetActiveWindowTitle();
+            previousActiveWindowTitle = activeWindowTitle;
+
+            DateTime activatedWindowDateTime = DateTime.Now;
+
+            Console.WriteLine($"Initial active window: {activeWindowTitle}\nActivated at: {activatedWindowDateTime.ToString("yyyy-MM-dd HH:mm:ss")}");
+
+            while (true)
+            {
+                activeWindowTitle = GetActiveWindowTitle();
+                Debug.WriteLine(activeWindowTitle);
+                if (!string.IsNullOrEmpty(activeWindowTitle) && previousActiveWindowTitle != activeWindowTitle)
+                {
+                    DateTime windowActiveEndDateTime = DateTime.Now;
+                    TimeSpan windowActiveTimeSpan = windowActiveEndDateTime - activatedWindowDateTime;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"Active window switched to: {activeWindowTitle}");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Activated at: { activatedWindowDateTime.ToString("yyyy-MM-dd HH:mm:ss")}");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Switched at: { windowActiveEndDateTime}");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Previous window active time: { windowActiveTimeSpan}");
+
+                    previousActiveWindowTitle = activeWindowTitle;
+                    activatedWindowDateTime = DateTime.Now;
+                }
+            }
+        }
+
+        private static string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+    }
+}
